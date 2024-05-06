f8a1824a7fae8929e775f913bc976e83eacbbd3c
eleoncmd <nora.shevketova@mail.ru>
2024-05-07 01:02:39 +0300

    Добавила новую функцию для алгоритма Дейкстры

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import json
 import random
 # import numpy as np
 # import matplotlib.image as img
-import heapq
-import sys
-from PIL import Image, ImageDraw, ImageColor
+# import heapq
+# import sys
+from PIL import Image, ImageDraw  # ,ImageColor
+
+
 # import json
 
 
@@ -39,7 +41,7 @@ def spliting(img_path):
 
 def initializing(i=0):
     """Инициализация изображения препятствиями (полигонами)"""
-    i = i+1
+    i = i + 1
     class_0 = []
     class_1 = []
     images = spliting('black_1.png')
@@ -50,7 +52,7 @@ def initializing(i=0):
 
         for j in range(polygon_numb):
             corner_numb = random.randint(3, 7)
-            corner_coordinates = tuple(random.randint(0, 512) for i in range(corner_numb*2))
+            corner_coordinates = tuple(random.randint(0, 512) for i in range(corner_numb * 2))
             # corner_coordinates = (j*10, (j*10+1), j, 0, 1, j*20)
 
             if corner_numb % 2 == 0:
@@ -68,7 +70,7 @@ def initializing(i=0):
     print('Препятствия 0:', *class_0)
     print('Препятствия 1:', *class_1)
 
-    new_img = Image.new('RGB', (img_width*2, img_height*2), (255, 255, 255))
+    new_img = Image.new('RGB', (img_width * 2, img_height * 2), (255, 255, 255))
     new_img.paste(images[0], (0, img_width))
     new_img.paste(images[1], (0, 0))
     new_img.paste(images[2], (img_width, 0))
@@ -88,19 +90,20 @@ def obstacle_data_1(coordinates, coors):
 def line_trow_polygon(x1, y1, x2, y2, x3, y3, x4, y4):
     dot = []
 
-    if ((y2-y1)*(x4-x3) - (y4-y3)*(x2-x1)) != 0:  # cчитаем определитель матрицы чтобы понять пересекаются линии или нет
-        print((y2-y1)*(x4-x3) - (y4-y3)*(x2-x1))
+    if ((y2 - y1) * (x4 - x3) - (y4 - y3) * (
+            x2 - x1)) != 0:  # cчитаем определитель матрицы чтобы понять пересекаются линии или нет
+        print((y2 - y1) * (x4 - x3) - (y4 - y3) * (x2 - x1))
         if (y2 - y1) != 0:  # Чтобы не было деления на ноль
-            q = (x2 - x1)/(y1-y2)
-            sn = (x3 - x4) + (y3 - y4)*q
+            q = (x2 - x1) / (y1 - y2)
+            sn = (x3 - x4) + (y3 - y4) * q
             if not sn:
                 return 0
             fn = (x3 - x1) + (y3 - y1) * q
-            n = fn/sn
+            n = fn / sn
         else:
             if not (y3 - y4):
                 return 0  # b(y)
-            n = (y3 - y1)/(y3 - y4)  # c(y) / b(y)
+            n = (y3 - y1) / (y3 - y4)  # c(y) / b(y)
 
         dot.append(x3 + (x4 - x3) * n)
         dot.append(y3 + (y4 - y3) * n)
@@ -115,13 +118,14 @@ def line_trow_polygon(x1, y1, x2, y2, x3, y3, x4, y4):
         print(dot[0], dot[1])
         return dot[0], dot[1]
 
+
 def obstacle_data_2(inp_coordinates, test_coordinates):
     """Получение данных о препятствиях - второй вариант"""
 
     info = {
-    'corner_numb': None,
-    'existence'  : None,
-    'coordinates': None
+        'corner_numb': None,
+        'existence': None,
+        'coordinates': None
     }
 
     if type(inp_coordinates) == "<class 'tuple'>":
@@ -143,104 +147,131 @@ def obstacle_data_2(inp_coordinates, test_coordinates):
         print(f.read())
 
 
-def new_metgod(coordinates):
-    coordinates_polygon = []
-    print(len(coordinates))
-    x1, y1 = 30, 20
-    x2, y2 = 1000, 1000
+
+def new_method1(start, finish, coordinates):
+    """Проверка на пересечение полигона линией"""
+
+    """Инициализация координат линии и создание объектов для отрисовки"""
+    x1, y1 = start[0], start[1]
+    x2, y2 = finish[0], finish[1]
     new_img1 = Image.new('RGB', (1024, 1024), (0, 0, 0))
     fig1 = ImageDraw.Draw(new_img1)
     fig2 = ImageDraw.Draw(new_img1)
-    for i in range(0, len(coordinates), 2):
-        print(i)
-        if (i + 3) < (len(coordinates)):
-            x3, y3 = coordinates[i], coordinates[i+1]
-            x4, y4 = coordinates[i+2], coordinates[i+3]
-            denominator = (y4 - y3) * (x1 - x2) - (x4 - x3) * (y1 - y2)
-            if denominator == 0:
-                if ((x1 * y2 - x2 * y1) * (x4 - x3) - (x3 * y4 - x4 * y3) * (x2 - x1) == 0) and ((x1 * y2 - x2 * y1) * (y4 - y3) - (x3 * y4 - x4 * y3) * (y2 - y1) == 0):
-                    print("Отрезки пересекаются(совпадают)")
-                    # return False
-                else:
-                    print('Отрезки не пересекаются(параллельны)')
-                    # return False
-            else:
-                numerator_a = (x4 - x2) * (y4 - y3) - (x4 - x3) * (y4 - y2)
-                numerator_b = (x1 - x2) * (y4 - y2) - (x4 - x2) * (y1 - y2)
-                Ua = numerator_a / denominator
-                Ub = numerator_b / denominator
-                if Ua >= 0 and Ua <= 1 and Ub >= 0 and Ub <= 1:
-                    x = x1 * Ua + x2 * (1 - Ua)
-                    y = y1 * Ua + y2 * (1 - Ua)
-                    print(x, "\n", y)
-                else:
-                    print('Не пересекаются')
-                    # return False
+    """Подготовка массива для более удобного перебора"""
+    coordinates = list(map(tuple, zip(coordinates[::2], coordinates[1::2])))
+    """Проверка на пересечение, совпадение, параллельность"""
+    for i in range(1, len(coordinates) + 1):
+        if i < len(coordinates):
+            x3, y3 = coordinates[i - 1][0], coordinates[i - 1][1]
+            x4, y4 = coordinates[i][0], coordinates[i][1]
         else:
-            x3, y3 = coordinates[i], coordinates[i+1]
-            x4, y4 = coordinates[0], coordinates[1]
+            x3, y3 = coordinates[i - 1][0], coordinates[i - 1][1]
+            x4, y4 = coordinates[0][0], coordinates[0][1]
             # return 0
+        denominator = (y4 - y3) * (x1 - x2) - (x4 - x3) * (y1 - y2)
+        if denominator == 0:
+            if ((x1 * y2 - x2 * y1) * (x4 - x3) - (x3 * y4 - x4 * y3) * (x2 - x1) == 0) and (
+                    (x1 * y2 - x2 * y1) * (y4 - y3) - (x3 * y4 - x4 * y3) * (y2 - y1) == 0):
+                print("Отрезки пересекаются(совпадают)")
+                # return False
+            else:
+                print('Отрезки не пересекаются(параллельны)')
+                # return False
+        else:
+            numerator_a = (x4 - x2) * (y4 - y3) - (x4 - x3) * (y4 - y2)
+            numerator_b = (x1 - x2) * (y4 - y2) - (x4 - x2) * (y1 - y2)
+            Ua = numerator_a / denominator
+            Ub = numerator_b / denominator
+            if Ua >= 0 and Ua <= 1 and Ub >= 0 and Ub <= 1:
+                x = x1 * Ua + x2 * (1 - Ua)
+                y = y1 * Ua + y2 * (1 - Ua)
+                print(x)
+                print(y)
+            else:
+                print('Не пересекаются')
+                # return False
+
+        print(f'x{i-1}:', x3, f'y{i-1}:', y3)
+        print(f'x{i}:', x4, f'y{i}:', y4)
 
         """Отрисовка полигона с линией"""
         fig2.polygon((x3, y3, x4, y4), (255, 255, 255))
         fig1.polygon((x1, y1, x2, y2), (255, 255, 255))
         new_img1.save('Polygon_line1.png')
-        new_img1.show()
+
+    new_img1.show()
+    return new_img1
 
 
 # def duild_vertexes(coordinates):
 #     t = [0, 1]
-#     xy = []
-#     temp_vertex = [(coordinates[i]*256, coordinates[i-1]*256) for i in range(len(coordinates), 2)]
-#     for
+#     pixel_vertex = [(coordinates[i]*256, coordinates[i-1]*256) for i in range(0, len(coordinates), 2)]
+#     print(pixel_vertex[0])
+#     return 1
+
+# def dijkstra(graph, start):
+#     """Алгоритм Дейкстры"""
 #
+#     # Инициализация расстояний до вершин
+#     distances = [vertex*256 for vertex in graph]
+#     distances[start] = 0
+#
+#     # Используем heapq для хранения вершин и их расстояний
+#     priority_queue = [(0, start)]
+#
+#     while priority_queue:
+#         current_distance, current_vertex = heapq.heappop(priority_queue)
+#
+#         if current_distance > distances[current_vertex]:
+#             continue
+#
+#         for neighbor, weight in graph[current_vertex].items():
+#             distance = current_distance + weight
+#
+#             if distance < distances[neighbor]:
+#                 distances[neighbor] = distance
+#                 heapq.heappush(priority_queue, (distance, neighbor))
+#
+#     return distances
+#
+# # Пример графа
+# graph = {
+#     'A': {'B': 5, 'C': 3},
+#     'B': {'A': 5, 'C': 2, 'D': 1},
+#     'C': {'A': 3, 'B': 2, 'D': 6},
+#     'D': {'B': 1, 'C': 6}
+# }
+#
+# start_vertex = 'A'
+# result = dijkstra(graph, start_vertex)
 
-def dijkstra(graph, start):
-    """Алгоритм Дейкстры"""
+# for vertex, distance in result.items():
+#     print(f'Расстояние от вершины {start_vertex} до {vertex} = {distance}')
 
-    # Инициализация расстояний до вершин
-    distances = [vertex*256 for vertex in graph]
-    distances[start] = 0
 
-    # Используем heapq для хранения вершин и их расстояний
-    priority_queue = [(0, start)]
+def broot_force(start, finish, coordinates):
+    """Построение тракетории полета с помощью метода грубой силы"""
 
-    while priority_queue:
-        current_distance, current_vertex = heapq.heappop(priority_queue)
+    """Перевод координат в пиксели"""
+    start, finish = start * 256, finish * 256
 
-        if current_distance > distances[current_vertex]:
-            continue
+    coordinates_px = [(coordinates[i - 1] * 256 + 1, coordinates[i] * 256 + 1) for i in
+                      range(1, (len(coordinates) + 1), 2)]
 
-        for neighbor, weight in graph[current_vertex].items():
-            distance = current_distance + weight
+    """Проверка всего полигона на пересечение"""
+    graph = []
 
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(priority_queue, (distance, neighbor))
 
-    return distances
+# broot_force(1, 2, (4, 3, 5, 3))
 
-# Пример графа
-graph = {
-    'A': {'B': 5, 'C': 3},
-    'B': {'A': 5, 'C': 2, 'D': 1},
-    'C': {'A': 3, 'B': 2, 'D': 6},
-    'D': {'B': 1, 'C': 6}
-}
-
-start_vertex = 'A'
-result = dijkstra(graph, start_vertex)
-
-for vertex, distance in result.items():
-    print(f'Расстояние от вершины {start_vertex} до {vertex} = {distance}')
 
 c = initializing()
 
 # line_trow_polygon(60,1,17, 14,11,7,12,13)
 
-new_metgod(c[1][0])
-dijkstra(c[1][0], 0)
+new_method1((0, 0), (1024, 1024), c[1][0])
+# duild_vertexes(c[1][0])
+# dijkstra(c[1][0], 0)
 # print('это', c[0])
 # obstacle_data_1(input(), c[0])
 # obstacle_data_2(input(), c[0])
-
