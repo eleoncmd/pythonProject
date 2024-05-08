928595d3156bca638698d52eeb630feb21ba49ff
eleoncmd <nora.shevketova@mail.ru>
2024-05-08 13:19:41 +0300

    Добавила инициализацию матрицы смежности(matrix)

--- Algorithm.py
+++ Algorithm.py
@@ -1,12 +1,3 @@
-matrix = [[], []]
-for i in range(len(matrix)):
-    for j in range(len(matrix)):
-        if i == j:
-            matrix[i][j].append(0)
-        elif i == len(matrix) - 1:
-            matrix[i][j].append(1)
-        else:
-            matrix[i][i+1].append(1)
-            matrix[i][i-1].append(1)
-
-print()
+"""
+1.Заполнение матрицы инцидентности
+"""
